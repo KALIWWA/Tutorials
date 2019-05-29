1369fcfdfbab6c55385ad1a268dcb6b2b736e022
Michal Kalinowski <subaru.kali@gmail.com>
2019-05-29 11:16:06 +0200

    typo

--- ServletAnnotation/src/main/java/net/javatutorial/tutorials/ServletWithAnnotations.java
+++ ServletAnnotation/src/main/java/net/javatutorial/tutorials/ServletWithAnnotations.java
@@ -1,12 +1,11 @@
 package net.javatutorial.tutorials;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "simpleServlet", urlPatterns = { "/hello" }, loadOnStartup = 1)
 public class ServletWithAnnotations extends HttpServlet {
@@ -14,7 +13,7 @@ public class ServletWithAnnotations extends HttpServlet {
 	private static final long serialVersionUID = -3462096228274971485L;
 
 	@Override
-	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.getWriter().println("Hello World!");
 	}
